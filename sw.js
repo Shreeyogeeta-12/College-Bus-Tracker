fda8272c6803af82f31ce97161ceb26fc5135d32
Shreeyogeeta-12 <yogeeta217@gmail.com>
2026-05-21 08:51:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,7 @@
-const CACHE_NAME = 'bus-tracker-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/driver.html'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
