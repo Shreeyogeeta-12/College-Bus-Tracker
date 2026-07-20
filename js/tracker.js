9bf563a7ccd036bf6aa41fb2d82839c72409d920
Shreeyogeeta-12 <yogeeta217@gmail.com>
2026-07-20 09:06:33 +0530

    Fix indentation in busMarker creation

--- js/tracker.js
+++ js/tracker.js
@@ -327,24 +327,24 @@ window.selectBus = function () {
     }
 
     if (!busMarker) {
-      busMarker = L.marker([data.lat, data.lng], {
-  icon: L.divIcon({
-    html: `
-      <div style="position:relative;width:44px;height:56px;text-align:center;">
-        <svg width="44" height="56" viewBox="0 0 44 56" xmlns="http://www.w3.org/2000/svg">
-          <path d="M22 2 C11 2 2 11 2 22 C2 36 22 54 22 54 C22 54 42 36 42 22 C42 11 33 2 22 2Z" fill="#dc2626" stroke="white" stroke-width="2"/>
-          <circle cx="22" cy="21" r="13" fill="white"/>
-        </svg>
-        <div style="position:absolute;top:6px;left:50%;transform:translateX(-50%);font-size:19px;line-height:1;">🚌</div>
-      </div>
-    `,
-    className: '',
-    iconSize:  [44, 56],
-    iconAnchor:[22, 54],
-  }),
-}).addTo(map);
-      map.setView([data.lat, data.lng], 15);
-    }
+          busMarker = L.marker([data.lat, data.lng], {
+            icon: L.divIcon({
+              html: `
+                <div style="position:relative;width:44px;height:56px;text-align:center;">
+                  <svg width="44" height="56" viewBox="0 0 44 56" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M22 2 C11 2 2 11 2 22 C2 36 22 54 22 54 C22 54 42 36 42 22 C42 11 33 2 22 2Z" fill="#dc2626" stroke="white" stroke-width="2"/>
+                    <circle cx="22" cy="21" r="13" fill="white"/>
+                  </svg>
+                  <div style="position:absolute;top:6px;left:50%;transform:translateX(-50%);font-size:19px;line-height:1;">🚌</div>
+                </div>
+             `,
+              className: '',
+              iconSize:  [44, 56],
+              iconAnchor:[22, 54],
+            }),
+          }).addTo(map);
+          map.setView([data.lat, data.lng], 15);
+        }
 
     // Sync stop index from driver panel
     if (typeof data.stopIndex === 'number') {
