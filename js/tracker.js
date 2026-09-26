674f1d0e2e73eb9396335e2bca188b2928498770
Shreeyogeeta-12 <yogeeta217@gmail.com>
2026-09-26 09:14:11 +0530

    Restore previous tracker.js code

--- js/tracker.js
+++ js/tracker.js
@@ -424,7 +424,7 @@ window.selectBus = function () {
       }
     }
 
-    if (!data || !data.lat || !data.lng || data.isActive === false) {
+    if (!data || !data.lat || !data.lng) {
       document.getElementById('info').innerText = '🔴 Bus is currently OFFLINE';
       if (etaLineEl) etaLineEl.innerText = '';
       if (busMarker) map.removeLayer(busMarker);
